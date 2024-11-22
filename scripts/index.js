a731ae399d9949ba43fcccdf5a380406eab23a9b
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2024-11-22 15:02:29 +0100

    Uploading Layout

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// let parentDiv =document.querySelector(".content");
+
+// let birdCassowary = document.createElement("div");
+// mainContent.classList.add("testdiv");
+// mainContent.textContent = "Hello this is generated via Script";
+// parentDiv.appendChild(mainContent);
+
+// document.querySelector(".testdiv").style.color = "#fff";
+
